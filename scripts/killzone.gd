6db2fc35dfd94e3741c06bcafc878ce6a70023dd
mohamed-ra5a <mohamed.mahany.ra5a@gmail.com>
2026-05-20 03:43:54 +0300

    creating the slime and finishing its animations and directions.

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,6 +5,8 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You died ")
+	Engine.time_scale = 0.5
+	body.get_node("CollisionShape2D").queue_free()
 	timer.start()
 	
 	
@@ -12,4 +14,6 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1
+
 	get_tree().reload_current_scene()
